1ef81b03a84456169a0148520df32062b1823734
alan simon <alancarlsimon@gmail.com>
2016-09-17 16:35:36 -0400

    Added login fragment layout. inc cardview in gradle compile

---
+++ app/src/main/java/codebusters/laddr/LoginActivity.java
@@ -0,0 +1,17 @@
+package codebusters.laddr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String LOGIN_A_TAG = "LOGIN_ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/codebusters/laddr/LoginFragment.java
@@ -0,0 +1,10 @@
+package codebusters.laddr;
+
+import android.app.Fragment;
+
+/**
+ * Created by alansimon on 2016-09-17.
+ */
+public class LoginFragment extends Fragment{
+
+}
